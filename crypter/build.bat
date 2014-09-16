9542960f9682da0d72eea73afdbe1564f9063c34
Ludo Ruisch <admin@ludoruisch.nl>
2014-09-16 07:48:24 +0200

    Final version

--- crypter/build.bat
+++ crypter/build.bat
@@ -4,11 +4,10 @@ g++ main.cpp -o main.exe -static
 
 pause
 
-::set project=demo
-::main.exe -e "%project%/input.txt" "%project%/input_key.txt" "%project%/output.txt" "%project%/output_key.txt"
-
 set project=crackme
-main.exe -e "%project%/crackme.zip" "%project%/input_key.txt" "%project%/crackme.zip.enc" "%project%/output_key.txt"
-::main.exe -d "%project%/crackme.zip.enc" "%project%/input_key.txt" "%project%/crackme.zip" "%project%/output_key.txt"
+::Encrypt
+::main.exe -e "%project%/crackme.zip" "%project%/input_key.txt" "%project%/crackme.zip.enc" "%project%/output_key.txt"
+::Decrypt
+main.exe -d "%project%/crackme.zip.enc" "%project%/input_key.txt" "%project%/crackme.zip" "%project%/output_key.txt"
 
 pause
